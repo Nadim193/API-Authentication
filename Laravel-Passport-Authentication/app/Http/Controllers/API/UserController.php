187fdf0d50a5470649663a3b15dc7eb6f0bcf743
Nadim Hossain <nadim.hossain.nh18.nh.nh@gmail.com>
2023-07-03 03:46:06 +0600

    Authentication Done

---
+++ Laravel-Passport-Authentication/app/Http/Controllers/API/UserController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\Token;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Passport\TokenRepository;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function userlogin(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = $request->user();
+            $token = $user->createToken('auth_token')->accessToken;
+            return response()->json(['user' => $user, 'token' => $token], 200);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+    public function registersubmit(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['user' => $user, 'message' => 'User registered successfully'], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function getuserdetails()
+    {
+        if(Auth::guard('api')->check()){
+            return response()->json(['user' => Auth::guard('api')->user()], 200);
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function userlogout(Request $request)
+    {
+        if (Auth::guard('api')->check()) {
+            $accessToken = Auth::guard('api')->user()->token();
+    
+            DB::table('oauth_access_tokens')
+                ->where('id', $accessToken->id)
+                ->update([
+                    'revoked' => true
+                ]);
+    
+            return response()->json(['message' => 'User logged out successfully'], 200);
+        }
+    
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
