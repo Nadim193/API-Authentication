e2f69047e111679a79a952e4a4a29151e93ff3f9
Nadim Hossain <nadim.hossain.nh18.nh.nh@gmail.com>
2023-07-03 03:56:03 +0600

    Authentication Done

---
+++ Laravel-Ui-Authentication/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+{
+
+    try {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = $request->user();
+            $token = $user->createToken('auth_token')->accessToken;
+            return response()->json(['user' => $user, 'token' => $token], 200);
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
+    
+        // return the $apiToken to the user or use it for further API requests
+    } catch (ValidationException $e) {
+        // handle authentication errors
+    }
+}
+
+}
